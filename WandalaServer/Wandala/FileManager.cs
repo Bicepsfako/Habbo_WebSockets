77308acb767af0c08c668a7bafe73ccde5bdbf6b
João Santos <joaocovaes@gmail.com>
2017-04-23 09:33:58 -0400

    Added File Manager

---
+++ WandalaServer/Wandala/FileManager.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Wandala
+{
+    class FileManager
+    {
+        public void Log(string message)
+        {
+            File.WriteAllText("Wandala_ErrorLog.txt", message);
+        }
+    }
+}
