8407a4fe39dc61d7fd8e231f5a0a6cba88124b3f
João Santos <joaocovaes@gmail.com>
2017-04-23 09:33:03 -0400

    Dictionary change & Reading connection command

--- WandalaServer/Wandala/Program.cs
+++ WandalaServer/Wandala/Program.cs
@@ -1,9 +1,8 @@
 ﻿using Alchemy;
 using Alchemy.Classes;
 using System;
-using System.Collections.Concurrent;
+using System.IO;
 using System.Collections.Generic;
-using System.Linq;
 using System.Net;
 using MySql.Data.MySqlClient;
 using Newtonsoft.Json;
@@ -18,12 +17,13 @@ namespace Wandala
         // MySQL Configuration
         static string connString = "Server=localhost;Database=habbo;Uid=root;Pwd=2296agosto";
         static MySqlConnection connection = new MySqlConnection(connString);
-        MySqlCommand command = connection.CreateCommand();
+        static MySqlCommand command = connection.CreateCommand();
         // Online users list
-        public static readonly ConcurrentDictionary<User, bool> OnlineUsers = new ConcurrentDictionary<User, bool>();
+        public static readonly Dictionary<UserContext, int> OnlineUsers = new Dictionary<UserContext, int>(); // user, user_id
+        public static readonly Dictionary<int, UserContext> UsersInRoom = new Dictionary<int, UserContext>(); // room_id, user
         static void Main(string[] args)
         {
-            Console.Title = "Wandala Server v0.0.1 | Port: " + port;
+            Console.Title = "Wandala Server v0.0.1 | Port: " + port + " | Online: 0 | Rooms Loaded: 0";
             Console.ForegroundColor = ConsoleColor.Magenta;
             Console.WriteLine("                                     __  ");
             Console.WriteLine(" \\            /    /\\      |\\   |   |  \\      /\\      |          /\\");
@@ -32,8 +32,6 @@ namespace Wandala
             Console.WriteLine("    \\/    \\/    /      \\   |   \\|   |__/   /      \\   |____   /      \\   v0.0.1");
             Console.ForegroundColor = ConsoleColor.Yellow;
             Console.WriteLine("\nUsing Alchemy Websockets - v2.0.0");
-            Console.ForegroundColor = ConsoleColor.Cyan;
-            Console.WriteLine("[Programmer] xSmoking");
             Console.ForegroundColor = ConsoleColor.White;
             Console.Write("Initializing server on port ");
             Console.ForegroundColor = ConsoleColor.Green;
@@ -59,28 +57,97 @@ namespace Wandala
             Console.ForegroundColor = ConsoleColor.Green;
             Console.WriteLine("Connected: " + context.ClientAddress);
             Console.ForegroundColor = ConsoleColor.White;
-            var client = new User(context);
-            OnlineUsers.TryAdd(client, false);
+            OnlineUsers.Add(context, 0);
+            Console.Title = "Wandala Server v0.0.1 | Port: " + port + " | Online: " + OnlineUsers.Count + " | Rooms Loaded: 0";
         }
 
         private static void OnReceive(UserContext context)
         {
             Console.WriteLine("Received data from: " + context.ClientAddress);
-            User client = new User(context);
             string data = context.DataFrame.ToString();
-            Console.WriteLine("Data: " + data);
+            string[] words = data.Split('|');
+            string key = null, value = null;
+            int count = 0;
+            foreach(string word in words)
+            {
+                if (0 == count) key = word;
+                else if (1 == count) value = word;
+                ++count;
+            }
+            switch (key)
+            {
+                case "new_connection":
+                    {
+                        try
+                        {
+                            connection.Open();
+                            command.CommandText = "UPDATE `users` SET online = '1' WHERE id = @VALUE;";
+                            command.Parameters.AddWithValue("VALUE", value);
+                            command.ExecuteNonQuery();
+                            OnlineUsers[context] = int.Parse(value);
+                        }
+                        catch (IOException e)
+                        {
+                            Console.WriteLine(e);
+                        }
+                        finally
+                        {
+                            if (connection.State == ConnectionState.Open)
+                                connection.Close();
+                        }
+                    }
+                    break;
+                case "load_room":
+                    {
+                        try
+                        {
+                            connection.Open();
+                            command.CommandText = "SELECT * FROM rooms WHERE id = @VALUE;";
+                            command.Parameters.AddWithValue("VALUE", value);
+                            MySqlDataReader reader = command.ExecuteReader();
+                            OnlineUsers[context] = int.Parse(value);
+                        }
+                        catch (IOException e)
+                        {
+                            Console.WriteLine(e);
+                        }
+                        finally
+                        {
+                            if (connection.State == ConnectionState.Open)
+                                connection.Close();
+                        }
+                    }
+                    break;
+                default:
+                    break;
+            }
         }
 
         private static void OnSend(UserContext context)
         {
-            var client = new User(context);
         }
 
         private static void OnDisconnect(UserContext context)
         {
-            var client = new User(context);
-            bool outBool;
-            OnlineUsers.TryRemove(client, out outBool);
+            try
+            {
+                int user_id;
+                OnlineUsers.TryGetValue(context, out user_id);
+                connection.Open();
+                command.CommandText = "UPDATE `users` SET online = '0' WHERE id = '" + user_id + "'";
+                command.ExecuteNonQuery();
+            }
+            catch (IOException e)
+            {
+                Console.WriteLine(e);
+            }
+            finally
+            {
+                if (connection.State == ConnectionState.Open)
+                    connection.Close();
+            }
+            OnlineUsers.Remove(context);
+            Console.Title = "Wandala Server v0.0.1 | Port: " + port + " | Online: " + OnlineUsers.Count + " | Rooms Loaded: 0";
             Console.ForegroundColor = ConsoleColor.Red;
             Console.WriteLine("Disconnected: " + context.ClientAddress.ToString());
             Console.ForegroundColor = ConsoleColor.White;
